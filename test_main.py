dbf9a5acf740a7c2446f0c48b742a9eb5abd42e6
LeninGF <enteatenea@gmail.com>
2019-06-16 00:38:54 -0500

    modifico el programa para probar con todos los datos

--- test_main.py
+++ test_main.py
@@ -6,25 +6,63 @@ Date: 13 June 2019
 '''
 
 import numpy as np
+import os
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input, decode_predictions
 from keras.preprocessing import image
 from keras.models import load_model
 
-new_model = load_model('cat-dog-horse-human.h5')
-print(new_model.summary())
 
 img_path = './test_cadohohu/horse.jpg'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x,axis=0)
-x = preprocess_input(x)
-print('Input Image Shape: ', x.shape)
 #%% md
 ## Predictions:
 # We print top 3 predictions with their % of probability
 #%%
-preds = new_model.predict(x)
-# print('Predicted: ', decode_predictions(preds, top=3)[0])
-print(preds)
 
+# print('Predicted: ', decode_predictions(preds, top=3)[0])
+
+
+def process_image(path2img):
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    print('Input Image Shape: ', x.shape)
+    return x
+
+
+def prediction(model, path2_folder, img_name):
+    img_path = os.path.join(path2_folder, img_name)
+    print(img_path)
+    x = process_image(path2img=img_path)
+    preds = model.predict(x)
+    return preds
+
+
+def main(path_to_h5, path_to_test_folder):
+    # Loading model from h5 file
+    new_model = load_model(path_to_h5)
+    print(new_model.summary())
+    # Prediction
+    y1 = prediction(new_model, path_to_test_folder, img_name='cat1.jpg')
+    print(y1)
+    y1 = prediction(new_model, path_to_test_folder, img_name='cat2.jpg')
+    print(y1)
+    y1 = prediction(new_model, path_to_test_folder, img_name='cat3.jpg')
+    print(y1)
+
+    y2 = prediction(new_model, path_to_test_folder, img_name='dog1.jpg')
+    print(y2)
+    y2 = prediction(new_model, path_to_test_folder, img_name='dog2.jpg')
+    print(y2)
+    y2 = prediction(new_model, path_to_test_folder, img_name='dog3.jpg')
+    print(y2)
+
+    y3 = prediction(new_model, path_to_test_folder, img_name='horse.jpg')
+    print(y3)
+    
+    return 0
+
+
+if __name__ == '__main__':
+    main(path_to_h5='cat-dog-horse-human.h5', path_to_test_folder='./test_cadohohu')
